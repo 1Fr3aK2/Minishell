6ddf04b7a651b8238cd48bbd3fc571164e7dd1ab
Rafael Matos <rafael.joao.amorim.matos@gmail.com>
2025-02-11 21:04:39 +0000

    added

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -3,86 +3,88 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: raamorim <raamorim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 12:55:03 by raamorim          #+#    #+#             */
-/*   Updated: 2025/02/11 18:23:13 by raamorim         ###   ########.fr       */
+/*   Updated: 2025/02/11 21:03:20 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/shellinho.h"
 
-// bool	is_quote(char c)
-// {
-// 	return ((c == '\'' || c == '\"'));
-// }
+bool	is_quote(char c)
+{
+	return ((c == '\'' || c == '\"'));
+}
 
-// int	count_skip_quotes(char *input)
-// {
-// 	int	quotes;
-// 	int	i;
+int	count_skip_quotes(char *input)
+{
+	int	quotes;
+	int	i;
 
-// 	if (!input)
-// 		return (0);
-// 	i = 0;
-// 	quotes = 0;
-// 	while (input[i])
-// 	{
-// 		while (is_space(input[i]))
-// 			i++;
-// 		if (is_quote(input[i]))
-// 		{
-// 			i++;
-// 			quotes++;
-// 		}
-// 		i++;
-// 	}
-// 	return (quotes);
-// }
+	if (!input)
+		return (0);
+	i = 0;
+	quotes = 0;
+	while (input[i])
+	{
+		if (is_quote(input[i]))
+			quotes++;
+		i++;
+	}
+	return (quotes);
+}
 
-// static char	*new_input(char *input)
-// {
-// 	int		i;
-// 	char	*new;
-// 	int		len;
-// 	int		j;
+static char	*new_input(char *input)
+{
+	char	*new;
+	int		i;
+	int		len;
+	int		quotes;
+	int		j;
 
-// 	i = 0;
-// 	printf("initial input = %s\n", input);
-// 	if (count_skip_quotes(input) == 0)
-// 		return (input);
-// 	if (count_skip_quotes(input) % 2 != 0)
-// 	{
-// 		printf("ERROR\n");
-// 		return (NULL);
-// 	}
-// 	len = ft_strlen(input);
-// 	while (input[i] && !(is_quote(input[i])))
-// 		i++;
-// 	j = ++i;
-// 	new = ft_substr(input, j, (len - j - 1));
-// 	if (!new)
-// 		return (input);
-// 	return (new);
-// }
+	j = 0;
+	i = -1;
+	printf("initial input = %s\n", input);
+	len = ft_strlen(input);
+	quotes = count_skip_quotes(input);
+	if (quotes == 0)
+		return (input);
+	if (quotes % 2 != 0)
+	{
+		printf("ERROR\n");
+		return (NULL);
+	}
+	new = (char *)malloc(sizeof(char) * (len - quotes + 1));
+	if (!new)
+		return (NULL);
+	while (input[++i])
+	{
+		if (!is_quote(input[i]))
+			new[j++] = input[i];
+	}
+	new[j] = '\0';
+	return (new);
+}
 
 void	parse(char *input, t_info *info)
 {
 	size_t	size;
-	// int		i;
+	char	*teste;
 
+	(void)info;
 	if (!input)
 		return ;
 	size = ft_strlen(input);
 	if (size > 0)
 	{
-		// i = count_skip_quotes(input);
-		// printf("%d\n", i);
-		// printf("1 carater arg 1 -> %c\n", input[5]);
-		info->args = ft_split(input);
+		teste = new_input(input);
+		if (teste)
+			printf("teste: %s\n", teste);
+		/* info->args = ft_split(input);
 		if (!info->args)
 			return ;
+		info->flags = ft_strdup(info->args[1]); */
 		// printf("1 carater arg 1 -> %c\n", info->args[1][0]);
-		info->flags = ft_strdup(info->args[1]);
 	}
 }
