169eab66744719a0540da46391e431c30dc350f7
dsteiger42 <167896122+dsteiger42@users.noreply.github.com>
2025-03-05 01:40:36 +0000

    Update ft_unset.c

--- src/builtins/ft_unset.c
+++ src/builtins/ft_unset.c
@@ -17,17 +17,17 @@ void    ft_unset(t_info *info)
     int i;
     int j;
 
-    i = 1;
+    i = 0;
     j = 0;
     if (info->args[1] == NULL)
         return ;
-    while (info->args[i])
+    while (info->args[i + 1])
     {
         while (info->my_env[j])
         {
-            if (info->args[i] == info->my_env[j])
+            if (ft_strncmp(info->args[i + 1], info->my_env[j], sizeof(info->my_env[j]) == 0)
             {
-            
+                ft_memset(
             }
         }
         j++;
