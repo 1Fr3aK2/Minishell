4d553fade6ec058fc314537879e332b3593f93e5
Rafael Matos <rafael.joao.amorim.matos@gmail.com>
2025-03-19 22:34:05 +0000

    added

--- src/builtins/ft_pipe.c
+++ src/builtins/ft_pipe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_pipe.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: raamorim <raamorim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 10:25:58 by raamorim          #+#    #+#             */
-/*   Updated: 2025/03/17 12:02:39 by raamorim         ###   ########.fr       */
+/*   Updated: 2025/03/19 19:31:54 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,39 +17,43 @@ static void exec_command(t_info *info, t_tree *node)
     if (!info || !node)
         return;
     info->cmd_tree = node;
+    if (check_builtins(info) == 0)
+        exit(0);
     exec(info);
+    ft_putstr_fd("Exec error", 2);
 }
 
 void ft_pipe(t_info *info, t_tree *node)
 {
-    int fd[2];
-    pid_t pid1, pid2;
+    int     fd[2];
+    pid_t   pid1;
+    pid_t   pid2;
 
     if (!info || !node || !node->left || !node->right)
-        return ;
+        return;
     if (pipe(fd) == -1)
-        ft_putstr_fd("Pipe error", 2);
+        return(ft_putstr_fd("Pipe error", 2));
+    if (check_builtins(info) == 0)
+        return;
     pid1 = fork();
     if (pid1 == -1)
-        ft_putstr_fd("Fork error", 2);
+        return (ft_putstr_fd("Fork error", 2));
     if (pid1 == 0)
     {
         close(fd[0]);
         dup2(fd[1], STDOUT_FILENO);
         close(fd[1]);
         exec_command(info, node->left);
-        ft_putstr_fd("Exec error", 2);
     }
     pid2 = fork();
     if (pid2 == -1)
-        ft_putstr_fd("Fork error", 2);
+        return (ft_putstr_fd("Fork error", 2));
     if (pid2 == 0)
     {
         close(fd[1]);
         dup2(fd[0], STDIN_FILENO);
         close(fd[0]);
         exec_command(info, node->right);
-        ft_putstr_fd("Exec error", 2);
     }
     close_pipe_fds(fd);
     waitpid(pid1, NULL, 0);
