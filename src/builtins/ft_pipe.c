101927e89be66be4040efbf61de6c29220e51f14
Rafael Matos <rafael.joao.amorim.matos@gmail.com>
2025-03-20 23:28:05 +0000

    added

--- src/builtins/ft_pipe.c
+++ src/builtins/ft_pipe.c
@@ -6,63 +6,89 @@
 /*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 10:25:58 by raamorim          #+#    #+#             */
-/*   Updated: 2025/03/19 19:31:54 by rafael           ###   ########.fr       */
+/*   Updated: 2025/03/20 23:27:12 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/shellinho.h"
 
-static void exec_command(t_info *info, t_tree *node)
+static void	exec_command(t_info *info, t_tree *node)
 {
-    if (!info || !node)
-        return;
-    info->cmd_tree = node;
-    if (check_builtins(info) == 0)
-        exit(0);
-    exec(info);
-    ft_putstr_fd("Exec error", 2);
+	if (!info || !node)
+		return ;
+	info->cmd_tree = node;
+	if (check_builtins(info) == 0)
+		exit(0);
+	exec(info);
+	ft_putstr_fd("Exec error", 2);
 }
 
 void ft_pipe(t_info *info, t_tree *node)
 {
-    int     fd[2];
-    pid_t   pid1;
-    pid_t   pid2;
+    int fd[2], prev_fd = -1;
+    pid_t pid;
+    t_tree *curr = node;
 
-    if (!info || !node || !node->left || !node->right)
-        return;
-    if (pipe(fd) == -1)
-        return(ft_putstr_fd("Pipe error", 2));
-    if (check_builtins(info) == 0)
-        return;
-    pid1 = fork();
-    if (pid1 == -1)
-        return (ft_putstr_fd("Fork error", 2));
-    if (pid1 == 0)
+    while (curr && curr->type == PIPE)
     {
-        close(fd[0]);
-        dup2(fd[1], STDOUT_FILENO);
+        if (pipe(fd) == -1)
+        {
+            ft_putstr_fd("Pipe error", 2);
+            return;
+        }
+        
+        pid = fork();
+        if (pid == -1)
+        {
+            ft_putstr_fd("Fork error", 2);
+            return;
+        }
+        if (pid == 0) // Child process
+        {
+            if (prev_fd != -1)
+            {
+                dup2(prev_fd, STDIN_FILENO);
+                close(prev_fd);
+            }
+            dup2(fd[1], STDOUT_FILENO);
+            close(fd[0]);
+            close(fd[1]);
+            exec_command(info, curr->left);
+        }
+        if (prev_fd != -1)
+            close(prev_fd);
         close(fd[1]);
-        exec_command(info, node->left);
+        prev_fd = fd[0];
+        curr = curr->right;
     }
-    pid2 = fork();
-    if (pid2 == -1)
-        return (ft_putstr_fd("Fork error", 2));
-    if (pid2 == 0)
+    // Last command in the pipe sequence
+    if (curr)
     {
-        close(fd[1]);
-        dup2(fd[0], STDIN_FILENO);
-        close(fd[0]);
-        exec_command(info, node->right);
+        pid = fork();
+        if (pid == -1)
+        {
+            ft_putstr_fd("Fork error", 2);
+            return;
+        }
+        if (pid == 0)
+        {
+            if (prev_fd != -1)
+            {
+                dup2(prev_fd, STDIN_FILENO);
+                close(prev_fd);
+            }
+            exec_command(info, curr);
+        }
     }
-    close_pipe_fds(fd);
-    waitpid(pid1, NULL, 0);
-    waitpid(pid2, NULL, 0);
+    if (prev_fd != -1)
+        close(prev_fd);
+    while (wait(NULL) > 0); // Wait for all child processes to complete
 }
+    
 
-void ft_pipe_wrapper(t_info *info)
+void	ft_pipe_wrapper(t_info *info)
 {
-    if (!info || !info->cmd_tree)
-        return;
-    ft_pipe(info, info->cmd_tree);
+	if (!info || !info->cmd_tree)
+		return ;
+	ft_pipe(info, info->cmd_tree);
 }
