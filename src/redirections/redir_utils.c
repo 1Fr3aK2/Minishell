71ee5d6bab9134fc2babba2fa0581a2a0cc1bbbe
Duarte Araujo <duartearaujo2002@gmail.com>
2025-03-27 17:57:45 +0000

    kaksj

--- src/redirections/redir_utils.c
+++ src/redirections/redir_utils.c
@@ -6,45 +6,43 @@
 /*   By: dsteiger <dsteiger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 15:52:25 by dsteiger          #+#    #+#             */
-/*   Updated: 2025/03/12 14:52:08 by dsteiger         ###   ########.fr       */
+/*   Updated: 2025/03/27 17:13:59 by dsteiger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/shellinho.h"
 
-void storing_backup(t_io *io)
+void	storing_backup(t_io *io)
 {
-    if (io)
-    {
-        io->stdin_backup = dup(STDIN_FILENO); // Backup stdin
-        io->stdout_backup = dup(STDOUT_FILENO); // Backup stdout
-        if (io->stdin_backup == -1 || io->stdout_backup == -1)
-            error_exit("dup failed: Could not backup stdin or stdout");
-    }
+	if (!io)
+		return ;
+	io->stdin_backup = dup(STDIN_FILENO);   // Backup stdin
+	io->stdout_backup = dup(STDOUT_FILENO); // Backup stdout
+	if (io->stdin_backup == -1 || io->stdout_backup == -1)
+		error_exit("dup failed: Could not backup stdin or stdout");
 }
 
 /*
-    Restores the original standard input and output
-    to their original fds of 0 and 1.
+	Restores the original standard input and output
+	to their original fds of 0 and 1.
 */
-void restore_io(t_io *io)
+void	restore_io(t_io *io)
 {
-    if (io)
-    {
-        if (dup2(io->stdin_backup, STDIN_FILENO) == -1) // STDIN now points to whatever file is stored in backup (which is fd(0))
-        {
-            close(io->stdin_backup);
-            close(io->stdout_backup);
-            error_exit("dup2 failed: Could not restore STDIN");
-        }
-        if (dup2(io->stdout_backup, STDOUT_FILENO) == -1) 
-        {
-            close(io->stdin_backup);
-            close(io->stdout_backup);
-            error_exit("dup2 failed: Could not restore STDOUT");
-        }
-        close(io->stdin_backup);
-        close(io->stdout_backup);
-    }
+	if (!io)
+		return ;
+	if (dup2(io->stdin_backup, STDIN_FILENO) == -1)
+	// STDIN now points to whatever file is stored in backup (which is fd(0))
+	{
+		close(io->stdin_backup);
+		close(io->stdout_backup);
+		error_exit("dup2 failed: Could not restore STDIN");
+	}
+	if (dup2(io->stdout_backup, STDOUT_FILENO) == -1)
+	{
+		close(io->stdin_backup);
+		close(io->stdout_backup);
+		error_exit("dup2 failed: Could not restore STDOUT");
+	}
+	close(io->stdin_backup);
+	close(io->stdout_backup);
 }
-
--- src/redirections/redirections.c
+++ src/redirections/redirections.c
@@ -3,131 +3,136 @@
 /*                                                        :::      ::::::::   */
 /*   redirections.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: raamorim <raamorim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dsteiger <dsteiger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 15:52:25 by dsteiger          #+#    #+#             */
-/*   Updated: 2025/03/24 17:54:30 by raamorim         ###   ########.fr       */
+/*   Updated: 2025/03/27 17:32:18 by dsteiger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/shellinho.h"
 
-
 // TO DO = parse the in_file and outfile
-void    handle_input_redirection(t_io *io)
+/*
+Nota: o dup2 e necessario pra duplicar o fd, porque,
+no caso do STDOUT, por exemplo, queremos redirecionar
+o output pra um ficheiro outfile.txt, inves do terminal.
+*/ 
+void	handle_input_redirection(t_io *io)
 {
-    if (!io && !io->in_file)
-        return ;
-    io->fd_in = open(io->in_file, O_RDONLY);
-    if (io->fd_in == -1)
-        error_exit("open failed in fd_in");
-    if (dup2(io->fd_in, STDIN_FILENO) == -1)
-    {
-        close(io->fd_in);
-        error_exit("dup2 failed: Could not redirect STDIN");
-    }
-    close(io->fd_in);
+	if (!io && !io->in_file)
+		return ;
+	io->fd_in = open(io->in_file, O_RDONLY);
+	if (io->fd_in == -1)
+		error_exit("open failed in fd_in");
+	if (dup2(io->fd_in, STDIN_FILENO) == -1)
+	{
+		close(io->fd_in);
+		error_exit("dup2 failed: Could not redirect STDIN");
+	}
+	close(io->fd_in);
 }
 
-void    handle_output_redirection(t_io *io)
+void	handle_output_redirection(t_io *io)
 {
-    if (!io && !io->out_file)
-        return ;
-    io->fd_out = open(io->out_file, O_WRONLY | O_CREAT | O_TRUNC, 0644); // O_TRUNC will overwritten the content
-    if (io->fd_out == -1)
-        error_exit("open failed in fd_out");
-    if (dup2(io->fd_out, STDOUT_FILENO) == -1)
-    {
-        close(io->fd_out);
-        error_exit("dup2 failed: Could not redirect STDOUT");
-    }
-    close(io->fd_out);
+	if (!io && !io->out_file)
+		return ;
+	io->fd_out = open(io->out_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (io->fd_out == -1)
+		error_exit("open failed in fd_out");
+	if (dup2(io->fd_out, STDOUT_FILENO) == -1)
+	{
+		close(io->fd_out);
+		error_exit("dup2 failed: Could not redirect STDOUT");
+	}
+	close(io->fd_out);
 }
 
-void    handle_append_redirection(t_io *io)
+void	handle_append_redirection(t_io *io)
 {
-    if (!io && !io->out_file)
-        return ;
-    io->fd_out = open(io->out_file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-    if (io->fd_out == -1)
-        error_exit("open failed in fd_out append");
-    if (dup2(io->fd_out, STDOUT_FILENO) == -1)
-    {
-        close(io->fd_out);
-        error_exit("dup2 failed: Could not redirect STDOUT append");
-    }
-    close (io->fd_out);
+	if (!io && !io->out_file)
+		return ;
+	io->fd_out = open(io->out_file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (io->fd_out == -1)
+		error_exit("open failed in fd_out append");
+	if (dup2(io->fd_out, STDOUT_FILENO) == -1)
+	{
+		close(io->fd_out);
+		error_exit("dup2 failed: Could not redirect STDOUT append");
+	}
+	close(io->fd_out);
 }
 
-void    handle_heredoc_redirection(t_io *io)
+void	handle_heredoc_redirection(t_io *io)
 {
-    char    *line;
-    int     pipe_fd[2];
+	char	*line;
+	int		fd[2];
 
-    if (!io || !io->here_delimiter)
-        return ;
-    if (pipe(pipe_fd) == -1)
-        error_exit("pipe failed for heredoc");
-    while (1)
-    {
-        line = readline("> ");
-        if (!line || ft_strncmp(line, io->here_delimiter, ft_strlen(io->here_delimiter)) == 0)
-        {
-            free(line);
-            break;
-        }
-        write(pipe_fd[1], line, ft_strlen(line));
-        write(pipe_fd[1], "\n", 1);
-        free(line);
+	if (!io || !io->here_delimiter)
+		return ;
+	if (pipe(fd) == -1)
+		error_exit("pipe failed for heredoc");
+	while (1)
+	{
+		line = readline("> ");
+		if (!line || ft_strncmp(line, io->here_delimiter,
+				ft_strlen(io->here_delimiter)) == 0)
+		{
+			free(line);
+			break ;
+		}
+		write(fd[1], line, ft_strlen(line));
+		write(fd[1], "\n", 1);
+		free(line);
     }
-    close(pipe_fd[1]);
-    if (dup2(pipe_fd[0], STDIN_FILENO) == -1)
-    {
-        close(pipe_fd[0]);
-        error_exit("dup2 failed: Could not redirect STDIN heredoc");
-    }
-    close(pipe_fd[0]);
+	close(fd[1]);
+	if (dup2(fd[0], STDIN_FILENO) == -1)
+	{
+		close(fd[0]);
+		error_exit("dup2 failed: Could not redirect heredoc");
+	}
+	close(fd[0]);
 }
 
-void handle_redirections(t_io *io, char **args)
+void	handle_redirections(t_io *io, char **args)
 {
-    int i;
-    
-    i = 0;
-    while (args[i])
-    {
-        if (strcmp(args[i], ">") == 0)
-        {
-            if (args[i + 1])
-            {
-                handle_output_redirection(io);
-                i++; // Skip the next argument as it's the filename
-            }
-        }
-        else if (strcmp(args[i], ">>") == 0)
-        {
-            if (args[i + 1])
-            {
-                handle_append_redirection(io);
-                i++;
-            }
-        }
-        else if (strcmp(args[i], "<") == 0)
-        {
-            if (args[i + 1])
-            {
-                handle_input_redirection(io);
-                i++;
-            }
-        }
-        else if (strcmp(args[i], "<<") == 0)
-        {
-            if (args[i + 1])
-            {
-                handle_heredoc_redirection(io);
-                i++;
-            }
-        }
-        i++;
-    }
+	int	i;
+
+	i = 0;
+	while (args[i])
+	{
+		if (strcmp(args[i], ">") == 0)
+		{
+			if (args[i + 1])
+			{
+				handle_output_redirection(io);
+				i++; // Skip the next argument as it's the filename
+			}
+		}
+		else if (strcmp(args[i], ">>") == 0)
+		{
+			if (args[i + 1])
+			{
+				handle_append_redirection(io);
+				i++;
+			}
+		}
+		else if (strcmp(args[i], "<") == 0)
+		{
+			if (args[i + 1])
+			{
+				handle_input_redirection(io);
+				i++;
+			}
+		}
+		else if (strcmp(args[i], "<<") == 0)
+		{
+			if (args[i + 1])
+			{
+				handle_heredoc_redirection(io);
+				i++;
+			}
+		}
+		i++;
+	}
 }
