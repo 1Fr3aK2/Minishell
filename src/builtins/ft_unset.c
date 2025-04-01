2e0a6799c2f71bb31b80419eae1dabf9a08165dd
Duarte Araujo <duartearaujo2002@gmail.com>
2025-04-01 17:47:16 +0100

    last

--- src/builtins/ft_unset.c
+++ src/builtins/ft_unset.c
@@ -6,42 +6,44 @@
 /*   By: dsteiger <dsteiger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 18:16:58 by dsteiger          #+#    #+#             */
-/*   Updated: 2025/03/24 15:26:22 by dsteiger         ###   ########.fr       */
+/*   Updated: 2025/04/01 17:41:32 by dsteiger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/shellinho.h"
 
-void    ft_unset(t_info *info)
+void	ft_unset(t_info *info)
 {
-    int i;
-    int j;
-    int k;
-    size_t len;
+	int i;
+	int j;
+	size_t len;
 
-    if (!info->cmd_tree->args[1])
-        return ;
-    i = 1;
-    while (info->cmd_tree->args[i])
-    {
-        j = 0;
-        len = ft_strlen(info->cmd_tree->args[i]);
-        while (info->my_env[j])
-        {
-            if (ft_strncmp(info->cmd_tree->args[i], info->my_env[j], len) == 0 && info->my_env[j][len] == '=')
-            {
-                free(info->my_env);
-                k = j;
-                while (info->my_env[k])
-                {
-                    info->my_env[k] = info->my_env[k + 1];
-                    k++;
-                }
-                info->my_env[k] = NULL;
-            }
-            else
-                j++;
-        }
-    }
-    i++;
+	if (!info->cmd_tree->args[1])
+		return ;
+	i = 1;
+	while (info->cmd_tree->args[i])
+	{
+		j = 0;
+		while (info->export_env[j])
+		{
+			len = ft_strlen(info->cmd_tree->args[i]);
+			if (ft_strncmp(info->cmd_tree->args[i], info->export_env[j],
+					len) == 0 && (info->export_env[j][len] == '='
+					|| info->export_env[j][len] == '\0'))
+			{
+				free(info->export_env[j]);
+                free(info->my_env[j]);
+				while (info->export_env[j + 1])
+				{
+					info->export_env[j] = info->export_env[j + 1];
+                    info->my_env[j] = info->my_env[j + 1];
+					j++;
+				}
+				info->export_env[j] = NULL;
+				break ;
+			}
+			j++;
+		}
+		i++;
+	}
 }
