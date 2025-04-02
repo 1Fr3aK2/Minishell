c773b4d6364d03f9fb80c5501d566c100d0238ff
Duarte Araujo <duartearaujo2002@gmail.com>
2025-04-02 23:52:20 +0100

    gogo

--- src/builtins/ft_unset.c
+++ src/builtins/ft_unset.c
@@ -28,67 +28,35 @@ void	ft_unset(t_info *info)
 		{
             len = ft_strlen(info->cmd_tree->args[i]);
 			if (ft_strncmp(info->cmd_tree->args[i], info->export_env[j],
-					len) == 0) //  && (info->export_env[j][len] == '='|| info->export_env[j][len] == '\0')
+					len) == 0)
 			{
 				free(info->export_env[j]);
-				//free(info->my_env[j]);
 				while (info->export_env[j + 1])
 				{
 					info->export_env[j] = info->export_env[j + 1];
-					//info->my_env[j] = info->my_env[j + 1];
 					j++;
 				}
 				info->export_env[j] = NULL;
-				//info->my_env[j] = NULL;
 				break ;
-			}
-			j++;
+            }
+            j++;
+        }
+        j = 0;
+        while (info->my_env[j])
+        {
+            if (ft_strncmp(info->cmd_tree->args[i], info->my_env[j], len) == 0)
+            {
+                free(info->my_env[j]);
+                while (info->my_env[j + 1])
+                {
+                    info->my_env[j] = info->my_env[j + 1];
+                    j++;
+                }
+                info->my_env[j] = NULL;
+                break ;
+            }
+		    j++;
 		}
 		i++;
 	}
 }
-
-/*
-void	ft_unset(t_info *info)
-{
-	int		i;
-	int		j;
-	int		nbr_envs;
-	size_t	len;
-
-	if (!info->cmd_tree->args[1])
-		return ;
-	i = 1;
-	nbr_envs = 0;
-	while (info->cmd_tree->args[i + nbr_envs])
-		nbr_envs++;
-	while (info->cmd_tree->args[i])
-	{
-		j = 0;
-		while (info->export_env[j + nbr_envs])
-		{
-			len = ft_strlen(info->cmd_tree->args[i]);
-			if (ft_strncmp(info->cmd_tree->args[i], info->export_env[j
-					+ nbr_envs],
-					len) == 0 && (info->export_env[j][len] == '='
-					|| info->export_env[j][len] == '\0'))
-			{
-				while (info->export_env[j + nbr_envs])
-				{
-					printf("%s\n", info->export_env[j + nbr_envs]);
-					free(info->export_env[j + nbr_envs]);
-					free(info->my_env[j + nbr_envs]);
-					info->export_env[j] = info->export_env[j + nbr_envs];
-					info->my_env[j] = info->my_env[j + nbr_envs];
-					j++;
-				}
-				info->export_env[j + nbr_envs] = NULL;
-				info->my_env[j + nbr_envs] = NULL;
-				nbr_envs--;
-			}
-			j++;
-		}
-		i++;
-	}
-}
- */
