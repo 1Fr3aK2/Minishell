f6b5751dd95397083dc8b8c7a83a9a3f67e99b4e
Rafael Matos <rafael.joao.amorim.matos@gmail.com>
2025-04-13 04:43:46 +0100

    added

--- src/builtins/ft_env.c
+++ src/builtins/ft_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_env.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: raamorim <raamorim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 18:55:29 by dsteiger          #+#    #+#             */
-/*   Updated: 2025/03/17 10:28:24 by raamorim         ###   ########.fr       */
+/*   Updated: 2025/04/13 04:29:04 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void    ft_env(t_info *info)
     i = 0;
     if (info->cmd_tree->args && info->cmd_tree->args[0] && info->cmd_tree->args[1])
         return ;
+    remove_all_quotes(info->my_env);
     while (info->my_env && info->my_env[i])
     {
         write(1, info->my_env[i], ft_strlen(info->my_env[i]));
