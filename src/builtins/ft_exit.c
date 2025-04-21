814cbcac64540d8cc78ef9cdbac707149ef9da6d
Duarte Araujo <duartearaujo2002@gmail.com>
2025-04-21 22:14:17 +0100

    lsl

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -14,17 +14,51 @@
 
 // sai do processo filo dentro de pipes cat Markefile | exit || CORRIGIT CTRL + D
 
+static int	ft_strisnum(char *str)
+{
+	size_t	count;
+
+	count = 0;
+	if (!ft_isdigit(str[count]) && str[count] != '-' && str[count] != '+')
+		return (0);
+	count++;
+	while (str[count])
+	{
+		if (!ft_isdigit(str[count]))
+			return (0);
+		count++;
+	}
+	return (1);
+}
+
 void	ft_exit(t_info *info)
 {
 	int	exit_flags;
+    int i;
+    int j;
 
 	exit_flags = ft_atoi(info->flags);
-/* 	if (info->cmd_tree->args[2])
-	{
-		printf("exit\nshellinho: exit: too many arguments\n");
-		exit_status = 1;
-		return ;
-	}	 */
+    i = 1;
+    if (info->cmd_tree->args[i])
+    {
+        j = 0;
+        if (info->cmd_tree->args[2] && ft_strisnum(info->cmd_tree->args[1]))
+        {
+            printf("exit\nshellinho: exit: too many arguments\n");
+            exit_status = 1;
+            return;
+        }
+        while (info->cmd_tree->args[i][j] != '\0')
+        {
+            if (!ft_isdigit(info->cmd_tree->args[i][j]))
+            {
+                printf("exit\nshellinho: exit: %s: numeric argument required\n", info->cmd_tree->args[i]);
+                exit_status = 2;
+                return;
+            }
+            j++;
+        }
+    }
 	if (isatty(STDIN_FILENO)) // checks if stdin is connected to a terminal. Ex: in input redirections, stdin is connected to a file
 		printf("exit\n");
 	if (exit_flags)
