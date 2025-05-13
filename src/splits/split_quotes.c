ac994804fa002b2023554112cd675dd53e7e9443
Rafael <rafael.joao.amorim.matos@gmail.com>
2025-05-13 18:36:49 +0100

    add

--- src/splits/split_quotes.c
+++ src/splits/split_quotes.c
@@ -5,36 +5,72 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: raamorim <raamorim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/17 14:23:09 by raamorim          #+#    #+#             */
-/*   Updated: 2025/05/13 17:35:17 by raamorim         ###   ########.fr       */
+/*   Created: 2025/05/13 16:43:51 by raamorim          #+#    #+#             */
+/*   Updated: 2025/05/13 18:29:27 by raamorim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/shellinho.h"
 
+static void	update_quotes(char c, int *in_quotes, char *quote)
+{
+	if (is_quote(c))
+	{
+		if (!*in_quotes)
+		{
+			*in_quotes = 1;
+			*quote = c;
+		}
+		else if (c == *quote)
+			*in_quotes = 0;
+	}
+}
+
 static int	get_word_length(char *str)
 {
-	int			len;
-	char		quote;
-	static int	in_quotes;
+	int		len;
+	int		in_quotes;
+	char	quote;
 
 	len = 0;
+	in_quotes = 0;
 	quote = 0;
-	while (str[len] && (!is_space(str[len]) || in_quotes == 1))
+	if (!str || !str[0])
+		return (0);
+	if (!in_quotes && is_operator_char(str[0]))
 	{
-		if (is_quote(str[len]))
-		{
-			if (!in_quotes)
-			{
-				in_quotes = 1;
-				quote = str[len];
-			}
-			else if (str[len] == quote)
-				in_quotes = 0;
-		}
-		len++;
+		if (is_double_operator(str))
+			return (2);
+		return (1);
 	}
+	while (str[len] && (!is_space(str[len]) && (!is_operator_char(str[len])
+				|| in_quotes)))
+		update_quotes(str[len++], &in_quotes, &quote);
 	return (len);
+}
+
+static int	count_tokens(char *s)
+{
+	int	i;
+	int	count;
+	int	len;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] && is_space(s[i]))
+			i++;
+		if (s[i])
+		{
+			len = get_word_length(s + i);
+			if (len == 0)
+				break ;
+			i += len;
+			count++;
+		}
+	}
+	return (count);
 }
 
 static int	word_alloc(char **dest, char *s, int len, int j)
@@ -57,7 +93,9 @@ char	**ft_split_quotes(char *s)
 
 	i = 0;
 	j = 0;
-	dest = (char **)malloc((count_word(s) + 1) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	dest = (char **)malloc((count_tokens(s) + 1) * sizeof(char *));
 	if (!dest)
 		return (NULL);
 	while (s[i])
@@ -68,8 +106,8 @@ char	**ft_split_quotes(char *s)
 		{
 			if (!word_alloc(dest, s + i, get_word_length(s + i), j))
 				return (free_str(dest, j));
-			j++;
 			i += get_word_length(s + i);
+			j++;
 		}
 	}
 	dest[j] = NULL;
