5f027d252a90b78eade3488c9c477cbbc5c01bcf
Rafael Matos <rafael.joao.amorim.matos@gmail.com>
2025-06-04 15:34:36 +0100

    add3D

--- src/processes/processes.c
+++ src/processes/processes.c
@@ -6,7 +6,7 @@
 /*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 11:53:00 by raamorim          #+#    #+#             */
-/*   Updated: 2025/06/04 15:32:55 by rafael           ###   ########.fr       */
+/*   Updated: 2025/06/04 15:34:01 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,39 +61,6 @@ static void	exec_child_process(t_info *info)
 	exec(info, info->cmd_tree);
 	exit(1);
 }
-
-/* void	child_process(t_info *info)
-{
-	pid_t	pid;
-	int		status;
-
-	if (!info)
-		return ;
-	storing_backup(info->io);
-	info->exit_status = 0;
-	prepare_heredocs(info->cmd_tree, info);
-	if (info->exit_status == 130)
-		return ;
-	check_redirections(info);
-	if (info->exit_status != 0)
-		return ;
-	set_signals_noninteractive();
-	free_io_file(info->io);
-	info->io->file = NULL;
-	close_io_fds(info->io);
-	if (!check_operators(info) || !check_builtins(info))
-		return (restore_io(info->io), (void)0);
-	pid = fork();
-	if (pid == -1)
-		return ;
-	if (pid == 0)
-		exec_child_process(info);
-	signal(SIGINT, SIG_IGN);
-	waitpid(pid, &status, 0);
-	set_signals_interactive();
-	restore_io(info->io);
-	handle_parent_signals(status, info);
-} */
 
 void	child_process(t_info *info)
 {
