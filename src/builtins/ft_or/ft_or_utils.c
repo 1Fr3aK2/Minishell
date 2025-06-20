daec3300f50cb933d0b8786dc2887f269b8f92cb
Rafael Matos <rafael.joao.amorim.matos@gmail.com>
2025-06-20 18:11:10 +0100

    add

--- src/builtins/ft_or/ft_or_utils.c
+++ src/builtins/ft_or/ft_or_utils.c
@@ -6,7 +6,7 @@
 /*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 14:15:58 by raamorim          #+#    #+#             */
-/*   Updated: 2025/06/20 16:40:14 by rafael           ###   ########.fr       */
+/*   Updated: 2025/06/20 18:06:06 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,13 +50,49 @@ static void	handle_status(t_info *info, int status)
 	}
 } */
 
+int is_builtin(char **builtins, char *cmd)
+{
+	int i;
+	i = 0;
+	while(builtins[i])
+	{
+		if (ft_strncmp(builtins[i], cmd, ft_strlen(builtins[i])) == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int exec_builtins(char *cmd, t_info *info)
+{
+
+	if (ft_strncmp(cmd, "cd", ft_strlen("cd")) == 0)
+		return (ft_cd(info), 0);
+	if (ft_strncmp(cmd, "echo", ft_strlen("echo")) == 0)
+		return (ft_echo(info), 0);
+	if (ft_strncmp(cmd, "env", ft_strlen("env")) == 0)
+		return (ft_env(info), 0);
+	if (ft_strncmp(cmd, "exit", ft_strlen("exit")) == 0)
+		return (ft_exit(info), 0);
+	if (ft_strncmp(cmd, "export", ft_strlen("export")) == 0)
+		return (ft_export(info), 0);
+	if (ft_strncmp(cmd, "unset", ft_strlen("unset")) == 0)
+		return (ft_unset(info), 0);
+	if (ft_strncmp(cmd, "pwd", ft_strlen("pwd")) == 0)
+		return (ft_pwd(info), 0);
+	return (1);
+}
+
 void	exec_command_op(t_info *info, t_tree *node)
 {
-	if (check_builtins(info) == 0)
+	if (is_builtin(info->builtins->builtins, node->args[0]) == 0)
 	{
-		close_heredoc_fd(info);
-		close_fds(0);
-		exit(info->exit_status);
+		if (exec_builtins(node->args[0], info) == 0)
+		{
+			close_heredoc_fd(info);
+			close_fds(0);
+			exit(info->exit_status);
+		}
 	}
 	exec(info, node);
 	close_heredoc_fd(info);
